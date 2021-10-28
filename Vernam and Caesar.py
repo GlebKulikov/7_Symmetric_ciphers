2a2d02835b82efa1cae6d600aa6c96002bed3669
Lost_garden <GlebKulikov1.0@gmail.com>
2021-10-28 22:04:28 +0300

    Add files via upload

---
+++ Vernam and Caesar.py
@@ -0,0 +1,56 @@
+from random import randint
+from re import findall
+
+
+def encrypt(k, m) -> object:
+    return ''.join([chr((ord(x) + k) % 65536) for x in m])
+
+
+def decrypt(k, m):
+    return ''.join([chr((ord(x) - k) % 65536) for x in m])
+
+
+def nonkey(m):
+    max_char = 0
+    key = 0
+    for i in range(65536):
+        tt = len(m.split(chr((32 + i) % 65536)))
+        if tt > max_char:
+            max_char = tt
+            key = i
+    return key
+
+
+def regular(text):
+    template = r"[0-9]+"
+    return findall(template, text)
+
+
+def vernam(m, tt='', k=''):
+    for i in m:
+        key = randint(0, 32);
+        k += str(key) + "/"
+        tt += chr((ord(i) + key - 16) % 32 + ord('А'))
+    return [tt, k]
+
+
+def dever(m, k, tt=''):
+    for index, i in enumerate(m):
+        tt += chr((ord(i) - int(regular(k)[index]) - 16) % 32 + ord('А'))
+    return tt
+
+
+# x=6
+# a=encrypt(x, 'Я люблю мёд, а вы любите мёд? Он отлично сочетается с зеленым чаем.')
+# print(a)
+# c=nonkey(a)
+# print(decrypt(x, a))
+# print(decrypt(c, a))
+
+
+# a = vernam('Мед это классно')
+# print(a[0])
+# b = dever(a[0], a[1])
+# print(b)
+
+# (Это комментарий) Я очень долго пытался разобраться с буквой "Р", которая почему-то стоит вместо пробелов в шифре вернама, но так и не смог :(
